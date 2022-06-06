870e2bf897999819974b831fdc22e6cc629edaa5
Adrien <adriennavarre@hotmail.com>
2022-06-06 13:08:58 -0700

    need to fix sphere

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
-import { Canvas, useFrame } from '@react-three/fiber'
+import React, { Suspense } from 'react'
+
+import { Canvas, useLoader } from '@react-three/fiber'
 import { OrbitControls, Stars } from '@react-three/drei';
+import { TextureLoader } from 'three';
+import texture from "./images/map.jpeg"
 import './App.css';
+import { Sphere } from 'three';
 
 function Box() {
+  const colorMap = useLoader(TextureLoader, texture)
+
   return (
-    <mesh position={[0,2,0]}>
+    <mesh position={[0,0,0]}>
       <boxBufferGeometry attach="geometry" />
-      <meshLambertMaterial attach="material" color="hotpink" />
+      <meshNormalMaterial attach="material" />
     </mesh>
   )
 }
 
-function Plane() {
+function AnimatedSphere() {
   return (
-    <mesh position={[0,0,0]} rotation={[-Math.PI / 2, 0, 0]}>
-      <planeBufferGeometry attach="geometry" args={[100, 100]}/>
-      <meshLambertMaterial attach="material" color="lightblue" />
-    </mesh>
+    <Sphere visible args={[1, 100, 200]} scale={2} ></Sphere>
   )
 }
 
 function App() {
   return (
     <div className="App">
-      <Canvas>
-        <OrbitControls />
+      <Canvas id='bg'>
+        <OrbitControls enableZoom={false}/>
         <Stars />
         <ambientLight intensity={0.5} />
         <spotLight position={[10,15,10]} angle={0.3} />
-        <Box />
-        <Plane />
+        <Suspense fallback={null}>
+          <Box />
+        </Suspense>
       </Canvas>
     </div>
   );
